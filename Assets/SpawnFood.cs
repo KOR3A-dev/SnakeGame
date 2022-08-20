d8ed4b246d0ce82cc8e9b6c9855aff2cfdc14bf9
Alejandro Valencia <68349791+KOR3A-dev@users.noreply.github.com>
2022-08-20 18:28:53 -0500

    adding boundaries in the world and implementing food

---
+++ Assets/SpawnFood.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFood : MonoBehaviour{
+
+    public GameObject foodPrefab;
+
+    public Transform borderTop;
+    public Transform borderBottom;
+    public Transform borderLeft;
+    public Transform borderRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
